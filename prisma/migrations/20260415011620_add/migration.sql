5890a92c5ba0dc305847d5b9993c0aecbbf345e6
Hoale.dev <leanhhoa30012004@gmail.com>
2026-04-15 08:28:22 +0700

    fix: correct table and column names to match casing conventions

--- prisma/migrations/20260415011620_add/migration.sql
+++ prisma/migrations/20260415011620_add/migration.sql
@@ -1,5 +1,5 @@
 -- AlterTable
-ALTER TABLE `post` MODIFY `content` VARCHAR(191) NULL;
+ALTER TABLE `Post` MODIFY `content` VARCHAR(191) NULL;
 
 -- AlterTable
-ALTER TABLE `user` ADD COLUMN `fatherName` VARCHAR(191) NULL;
+ALTER TABLE `User` ADD COLUMN `fatherName` VARCHAR(191) NULL;
